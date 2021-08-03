2b2a79ce9dbd67b39d6f7906317d541004ea204b
Andrey Dolgikh <lxnewayfarer@yandex.ru>
2021-08-04 02:39:37 +0700

    formatted

--- zip2pdf.py
+++ zip2pdf.py
@@ -3,62 +3,70 @@ from shutil import move, rmtree
 import zipfile
 from PIL import Image
 
+
 def unpack_zips_from_zip_folder(foldername="./zip"):
     zips = files_list_from_folder(foldername)
 
     counter = 0
 
-    print('ZIPs left: ', end='')
+    print("ZIPs left: ", end="")
     for zip in zips:
-        print('[ ]', end='')
-    print('\n___________', end='')
+        print("[ ]", end="")
+    print("\n___________", end="")
 
     # each archive
     for zip in sorted(zips):
-        with zipfile.ZipFile(foldername + '/' + zip, 'r') as zip_ref:
-            zip_ref.extractall('./temp')
+        with zipfile.ZipFile(foldername + "/" + zip, "r") as zip_ref:
+            zip_ref.extractall("./temp")
         # unziped to ./temp
-        temp_files = files_list_from_folder('./temp')
+        temp_files = files_list_from_folder("./temp")
         # now move and rename every file in temp folder to images with index [counter] name
 
         for file in sorted(temp_files):
-            file_extension = file.split('.')[-1]
-            move('temp/' + file, 'images/' + str(counter) + '.' + file_extension)
+            file_extension = file.split(".")[-1]
+            move("temp/" + file, "images/" + str(counter) + "." + file_extension)
             counter += 1
 
-        print('[✓]', end='')
-    print('\nDone. Check /images folder or result.pdf')
+        print("[✓]", end="")
+    print("\nDone. Check /images folder or result.pdf")
+
 
 def aliens():
-    print(open('.aliens', 'r').read(), end='\n')
+    print(open(".aliens", "r").read(), end="\n")
+
 
 def files_list_from_folder(foldername="./zip"):
     files = []
     for dirpath, dirnames, filenames in walk(foldername):
-        files.extend(filenames)    
+        files.extend(filenames)
         break
     return files
+
 
 def image_open(name):
     image = Image.open(name)
     try:
-        image.load() # required for png.split()
+        image.load()  # required for png.split()
         rgb = Image.new("RGB", image.size, (255, 255, 255))
-        rgb.paste(image, mask=image.split()[3]) # 3 is the alpha channel
+        rgb.paste(image, mask=image.split()[3])  # 3 is the alpha channel
     except Exception:
         return image
     return rgb
 
+
 def to_pdf():
-    images_names = files_list_from_folder('./images')
+    images_names = files_list_from_folder("./images")
     first_image = image_open("./images/" + images_names[0])
 
-    images = list(image_open("./images/" + image_name) for image_name in images_names[1:])
+    images = list(image_open("./images/" + image_name)
+                  for image_name in images_names[1:])
 
-    first_image.save("result.pdf", "PDF" ,resolution=100.0, save_all=True, append_images=images)
+    first_image.save("result.pdf", "PDF", resolution=100.0,
+                     save_all=True, append_images=images)
+
 
 def clear_folders():
-    try: 
+    try:
         rmtree("images")
     except Exception:
         pass
@@ -68,11 +76,12 @@ def clear_folders():
         pass
     mkdir("images")
 
-if __name__ == "__main__": 
+
+if __name__ == "__main__":
     aliens()
     clear_folders()
     unpack_zips_from_zip_folder()
     to_pdf()
 
-    print('Press [ENTER] to exit')
+    print("Press [ENTER] to exit")
     input()
